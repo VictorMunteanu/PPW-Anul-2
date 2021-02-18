dfb560c115faaa7076b2da838043b01a9f7d8469
Munteanu Victor <54933783+VictorMunteanu@users.noreply.github.com>
2021-02-18 11:14:09 +0200

    Add files via upload

---
+++ 18.02.21/JS/Main.js
@@ -0,0 +1,44 @@
+// Suma la 5 elemente
+
+
+
+// const array = [1, 2, 3, 4, 5];
+// const sum = array.reduce (function(x, y) {
+// 	return x + y;
+// }, 0);
+// alert(sum);
+
+// const array = [1, 2, 3, 4, 5];
+// let sum = 0;
+// array.forEach(element => {
+//     sum += element;
+// });
+// alert(sum);
+
+
+
+// Suma + media
+
+
+
+// const marks = [9.5, 8.33, 10, 7.05, 9.4];
+// const sum = marks.reduce (function(x, y) {
+// 	return x + y;
+// }, 0);
+// const average = sum/marks.length;
+// alert(average);
+
+// const marks = [4.5, 8.33, 10, 7.05, 9.4];
+// let sum = 0; 
+// marks.forEach(element => {
+//     if (element < 5) {
+//         alert("Restantier");
+//     } else {
+//         alert("An cu succes");
+//     }
+// });
+// marks.forEach(element => {
+//     sum += element;
+// });
+// const average = sum/marks.length;
+// alert(average);
